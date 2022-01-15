9d97e7b46aecab9f94775c1213f8860acf1cc3df
FanjaRj <95909791+FanjaRj@users.noreply.github.com>
2022-01-15 09:41:21 +0100

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -214,11 +214,10 @@ def shap_id(ID):
     shap.bar_plot(shap_vals[1][0],feature_names=X_name,max_display=10)
     #shap.force_plot(explainer.expected_value[1], shap_vals[1], app_id)
     
-def shap_all():    
-    app_all = app[X_name]
-    st.table(app_all)    
+def shap_all():
+    st.write(X_name)    
+    app_all = app[X_name] 
     shap_values = explainer.shap_values(app_all)
-    st.write('c')    
     shap.summary_plot(shap_values,feature_names=X_name,max_display=10)
 
 
